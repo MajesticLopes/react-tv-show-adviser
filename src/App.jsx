d0d2dce3bcb2a542238646737004f289c32afd32
Michael Lopes <gh api loopes.09@gmail.com>
2024-01-15 19:10:28 -0500

    Creation des composants

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./global.css";
+import s from "./style.module.css";
+
+export function App() {
+  return (
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_detail}>detail</div>
+      <div className={s.recommendations}>recommendations</div>
+    </div>
+  );
+}
