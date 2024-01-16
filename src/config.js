81c90323a0f3e8c152fa5be7008285921a4e12ac
Michael Lopes <gh api loopes.09@gmail.com>
2024-01-16 11:31:04 -0500

    Fetch, URL et background image

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=c418ac29340b2f6451cde1ca119a51c2";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+
+export {BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL};
